86f6eb226c5b60a81daa830123519f7c450b5990
Petra696 <91752761+Petra696@users.noreply.github.com>
2021-11-23 19:27:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('Hello Winc Academy students');
+// This is a large sum
+// Je ziet deze lijn niet, alleen de developer
+/* This is a comment block on muliple sums */
+// Je ziet deze lijn niet, alleen de developer
+let multiplication = 1230941 * 1823794;
+console.log("Multiplication:", multiplication);
+let division = 637263 / 54;
+console.log("Division:", division);
